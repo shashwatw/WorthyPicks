9b7c15549d7234cb51a5c370c42a67c2ad5d1102
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-28 09:25:30 +0530

    Usepopcorn project API fetching

--- src/App-v1.js
+++ src/App-v1.js
@@ -50,16 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "5095e0a9";
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=inception`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
   return (
     <>
       {/* component composition */}
