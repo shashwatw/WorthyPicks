b3a0a7e20c2d6ed8b50a225c3beca8f0ef30ca2f
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-28 09:45:21 +0530

    Using an async function

--- src/App.js
+++ src/App.js
@@ -55,11 +55,21 @@ const KEY = "5095e0a9";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "titanic";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=titanic`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      console.log(data.Search);
+
+      // .then((res) => res.json())
+      // .then((data) => setMovies(data.Search));
+    }
+    fetchMovies();
   }, []);
 
   return (
