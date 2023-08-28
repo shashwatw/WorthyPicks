5965d3f3a5a9333ceb2881dbce0690bc4db387ea
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-28 09:49:35 +0530

    Implemented Loader

--- src/App.js
+++ src/App.js
@@ -55,16 +55,18 @@ const KEY = "5095e0a9";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "titanic";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
 
       // .then((res) => res.json())
       // .then((data) => setMovies(data.Search));
@@ -90,9 +92,7 @@ export default function App() {
             </>
           }
         /> */}
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         {/* reusable box component */}
         <Box>
@@ -102,6 +102,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader"> Loading...</p>;
 }
 
 function NavBar({ children }) {
