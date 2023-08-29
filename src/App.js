8769d02f1fc75e245454702f6557bf64a61fab8c
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-08-29 11:06:38 +0530

    Cleaning up data fetchinG

--- src/App.js
+++ src/App.js
@@ -99,12 +99,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -114,9 +117,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie Not Found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.log(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError ") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -129,6 +135,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
